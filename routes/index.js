ab4ea1dec2ad0b6b49b963e89a18e33d08ed63dd
lumnious <dotico@gmail.com>
2016-06-27 15:02:10 +0100

    Completed POST route for '/'. Completed view to interact with user. Added autoreap to clear files after uploaded.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest:'./public/images/'});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'API Basejump: File Metadata Microservice' });
+  res.render('index', { title: 'API Basejump: File Metadata Microservice'});
+});
+
+router.post('/', upload.single('file'), function (req, res, next) {
+	console.log(req.file.size);
+	var out = '{"size": ' + req.file.size + '}';
+	res.render('index', { title: 'API Basejump: File Metadata Microservice'  , output: out});
 });
 
 module.exports = router;
